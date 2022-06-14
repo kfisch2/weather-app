eb85f87b953e5fae9fff636ff995004f411ddae5
kfisch2 <kmlindsey24@gmail.com>
2022-06-14 16:26:32 -0700

    convert city to lat and lon

--- assets/script.js
+++ assets/script.js
@@ -4,29 +4,45 @@ let userInput = document.querySelector(".userInput");
 // get weather button
 let weatherBtn = document.querySelector(".weatherBtn")
 weatherBtn.addEventListener("click", function () {
-  // divide user input into lat and lon
-  userInput.value.split(",")
-  let lat = userInput.value.split(", ")[0]
-  let lon = userInput.value.split(", ")[1]
-
-  getTemperature(lat, lon)
+  let userCity = userInput.value;
+  getLatAndLon(userCity)
 });
+
+// convert userCity to lat and lon
+let getLatAndLon = (city) => {
+  const APIKey = "1eb784753dd9691347d2b905eeeffc69";
+  const weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?&q=" + city + "&exclude=country&appid=" + APIKey;
+
+  fetch(weatherAPI).then(function (response) {
+    response.json().then(function (data) {
+      let lat = data.city.coord.lat;
+      let lon = data.city.coord.lon;
+      getTemperature(lat, lon)
+    });
+  });
+};
 
 
 // get weather function
 let getWeather = (lat, lon) => {
   const APIKey = "1eb784753dd9691347d2b905eeeffc69";
   const weatherAPI = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + '&exclude=hourly,minutely,alerts&appid=' + APIKey;
-
   fetch(weatherAPI).then(function (response) {
     response.json().then(function (data) {
+      // display current temperature
+      let temperature = document.createElement("div");
+      weatherEl.appendChild(temperature);
+      temperature.innerHTML = "The current temperature is " + data.current.temp + " degrees Fahrenheit";
       console.log(data)
+      for (i = 0; i < 5; i++) {
+
+        console.log(data.daily[i].temp)
+      }
     });
   });
 };
 
 // display temperature function
-
 let getTemperature = (lat, lon) => {
   const APIKey = "1eb784753dd9691347d2b905eeeffc69";
   const weatherAPI = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + '&exclude=hourly,minutely,alerts&appid=' + APIKey;
@@ -35,11 +51,6 @@ let getTemperature = (lat, lon) => {
     response.json().then(function (data) {
 
       let weatherEl = document.querySelector(".weatherInfo");
-
-      // display timezone
-      let timezone = document.createElement("div");
-      weatherEl.appendChild(timezone);
-      timezone.innerHTML = data.timezone;
 
       // display current temperature for timezone
       let temperature = document.createElement("div");
@@ -53,7 +64,7 @@ let getTemperature = (lat, lon) => {
         let fiveDayForecastTemp = document.createElement("div");
         fiveDayForecastTemp.className = "dailyWeather"
         weatherEl.appendChild(fiveDayForecastTemp);
-        fiveDayForecastTemp.innerHTML = "low of " + data.daily[i].temp.min + " and a high of " + data.daily[i].temp.max;     
+        fiveDayForecastTemp.innerHTML = "low of " + data.daily[i].temp.min + " and a high of " + data.daily[i].temp.max;
       };
 
 
@@ -74,17 +85,6 @@ let getTemperature = (lat, lon) => {
 
 //display weather conditions
 
-
-let getForecast = (city) => {
-  const APIKey = "1eb784753dd9691347d2b905eeeffc69";
-  const weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?&q=" + city + "&exclude=country&appid=" + APIKey;
-
-  fetch(weatherAPI).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data)
-    });
-  });
-};
 
 
 
